bb6759a4061dce5c21780a457f8ec54bf7cdb90e
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-12-01 17:05:46 +0100

    Update fun-odio.js

--- plugins/fun-odio.js
+++ plugins/fun-odio.js
@@ -1,6 +1,5 @@
 let handler = async (m, { conn, command, text }) => {
-    let nomeDelBot = global.db.data.nomedelbot || `𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲`
-  
+    
     let love = `──────────────\n𝐂𝐀𝐋𝐂𝐎𝐋𝐀𝐓𝐎𝐑𝐄 𝐃𝐈 𝐎𝐃𝐈𝐎 😡
   L'odio tra ${text} e te: ${Math.floor(Math.random() * 100)}%\n──────────────`.trim()
   
@@ -11,9 +10,9 @@ let handler = async (m, { conn, command, text }) => {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363259442839354@newsletter',
+          newsletterJid: '120363420674060561@newsletter',
           serverMessageId: '',
-          newsletterName: `${nomeDelBot}`
+          newsletterName: `${nomebot}`
         }
       }
     })
