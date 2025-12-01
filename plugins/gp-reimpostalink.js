2e46cec6f1c70514fb2c9a41b8494f653a4b583f
dieh <madycolavitti08@gmail.com>
2025-12-01 18:15:58 +0100

    Update gp-reimpostalink.js

--- plugins/gp-reimpostalink.js
+++ plugins/gp-reimpostalink.js
@@ -9,7 +9,7 @@ let handler = async(m, { conn }) => {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363259442839354@newsletter',
+          newsletterJid: '120363420674060561@newsletter',
           serverMessageId: '',
           newsletterName: `${nomeDelBot}`
         }
