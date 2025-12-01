6947894e64599fabe139dc6f472ec17a6cb83735
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-12-01 16:41:22 +0100

    Update fun-cornuto.js

--- plugins/fun-cornuto.js
+++ plugins/fun-cornuto.js
@@ -16,7 +16,7 @@ let handler = async (m, { conn, command, text }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363259442839354@newsletter',
+                    newsletterJid: '120363420674060561@newsletter',
                     newsletterName: '👑 Club dei Cornuti Ufficiale 👑'
                 }
             },
