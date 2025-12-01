f08363dccedc33e7af16795dbf2a087bced8d558
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-12-01 20:01:55 +0100

    Update owner-riavvio.js

--- plugins/owner-riavvio.js
+++ plugins/owner-riavvio.js
@@ -1,14 +1,14 @@
 const handler = async (m, { conn, isROwner, text }) => {
     const datas = global
     
-    if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+    
     const { key } = await conn.sendMessage(m.chat, {text: `Sto riavviando`}, {quoted: m})
     await delay(1000 * 1)
     await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀`, edit: key})
     await delay(1000 * 1)
     await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀🚀🚀`, edit: key})
     await conn.sendMessage(m.chat, {text: `Riavviato con successo`, edit: key})
-    //process.send('reset')
+    process.send('reset')
     process.exit(0); 
     }
     handler.help = ['riavvia'] 
