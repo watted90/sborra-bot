543ef7faaa5fd1e3feb5a17bc51e5b73e7edb17d
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-12-01 16:53:02 +0100

    Update fun-alcolizzato.js

--- plugins/fun-alcolizzato.js
+++ plugins/fun-alcolizzato.js
@@ -28,7 +28,7 @@ ${finalPhrase}
             forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363420674060561@newsletter',
                 serverMessageId: '',
-                newsletterName: `${nomeDelBot}` // Utilizzo della variabile botName
+                newsletterName: `${nomebot}` 
             },
         }
     };
