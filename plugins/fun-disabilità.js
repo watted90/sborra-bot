48832fab1278e78b4e379b78b38184e7eeca9b4a
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-12-01 17:00:43 +0100

    Update fun-disabilità.js

--- plugins/fun-disabilità.js
+++ plugins/fun-disabilità.js
@@ -50,7 +50,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363259442839354@newsletter',
+                newsletterJid: '120363420674060561@newsletter',
                 newsletterName: '🔥 ISTITUTO NAZIONALE IDIOZIA 🔥'
             }
         },
