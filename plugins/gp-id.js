ae719f1bd1fd71802aee53ab66606405c5a09310
dieh <madycolavitti08@gmail.com>
2025-12-01 18:01:57 +0100

    Update gp-id.js

--- plugins/gp-id.js
+++ plugins/gp-id.js
@@ -5,9 +5,9 @@ var handler = async (m, { conn, groupMetadata }) => {
             forwardingScore: 99,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363259442839354@newsletter',
+                newsletterJid: '120363420674060561@newsletter',
                 serverMessageId: '',
-                newsletterName: 'ChatUnity'
+                newsletterName: '${nomebot}'
             }
         }
     }, { quoted: m });
