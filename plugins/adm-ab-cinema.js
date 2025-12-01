4f6ee88b5f7a2251625edd9eb8a49fbbf6c6613f
dieh <madycolavitti08@gmail.com>
2025-12-01 19:25:28 +0100

    Create adm-ab-cinema.js

---
+++ plugins/adm-ab-cinema.js
@@ -0,0 +1,32 @@
+//Plugin fatto da Axtral_WiZaRd
+
+import fs from 'fs'
+
+let handler = message => message;
+
+handler.all = async function (message) {
+    if (!message.isGroup) return null;
+
+    let chatData = global.db.data.chats[message.chat];
+
+    if (!chatData.cinema) return null;
+
+    if (!message.text) return null;
+
+    if (/absolute cinema/i.test(message.text)) {
+        const stickerPath = './stickers/absolute-cinema.webp';
+        const stickerData = fs.readFileSync(stickerPath);
+
+        await conn.sendMessage(
+            message.chat,
+            { sticker: stickerData },
+            { quoted: message }
+        );
+    }
+};
+
+export default handler;
+
+function pickRandom(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
