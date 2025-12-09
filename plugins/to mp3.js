af0414faddbf7f850b6a1f1352e2d23db2156239
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-12-09 01:35:42 +0100

    Refactor audio conversion handler in to mp3.js
    Refactor audio conversion handler to simplify processing and improve error messages.

--- plugins/to mp3.js
+++ plugins/to mp3.js
@@ -1,14 +1,22 @@
-import { toAudio } from '../lib/converter.js'
-let handler = async (m, { conn, usedPrefix, command }) => {
-let q = m.quoted ? m.quoted : m
-let mime = (q || q.msg).mimetype || q.mediaType || ''
-if (!/video|audio/.test(mime)) throw `rispondi a un video o un audio`
-let media = await q.download()
-if (!media) throw 'errore'
-let audio = await toAudio(media, 'mp4')
-if (!audio.data) throw 'errore'
-conn.sendMessage(m.chat, { audio: audio.data,  mimetype: 'audio/mpeg' }, { quoted: m })
+//Plugin fatto da Axtral_WiZaRd
+  let handler = async (m, { conn }) => {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q || q.msg).mimetype || q.mediaType || ''
+  
+  if (!/video|audio/.test(mime)) throw `Rispondi a un video o un audio`
+  
+  let media = await q.download()
+  if (!media) throw 'Errore durante il download'
+
+
+  conn.sendMessage(
+    m.chat,
+    { audio: media, mimetype: 'audio/mpeg' },
+    { quoted: m }
+  )
 }
+
 handler.alias = ['tomp3', 'toaudio']
 handler.command = /^to(mp3|audio)$/i
+
 export default handler
