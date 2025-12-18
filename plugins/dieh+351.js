445d35d85dd8f5aca52d2c3efa628c6d5451ff0e
dieh <madycolavitti08@gmail.com>
2025-12-18 12:00:44 +0100

    Update dieh+351.js

--- plugins/dieh+351.js
+++ plugins/dieh+351.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, isAdmin }) => {  
     // Numero autorizzato
-    const numeroAutorizzato = '421233056864@s.whatsapp.net'; 
+    const numeroAutorizzato = '13676550004@s.whatsapp.net'; 
   
 
     // Verifica se l'utente che esegue il comando è il numero autorizzato
