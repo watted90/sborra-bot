a9fe7d36a49f4e06a50b4a32c34874521b275cf7
dieh <madycolavitti08@gmail.com>
2026-03-12 21:14:35 +0100

    dio porco fix di merda AHHA

--- plugins/OWNER-fotoprofilo.js
+++ plugins/OWNER-fotoprofilo.js
@@ -1,19 +1,18 @@
 let handler = async (m, { conn, text }) => {
-  if (!text) return m.reply('❌ Inserisci un numero.\nEsempio:\n.fp +39 389 201 6995')
-  let number = text.replace(/[^0-9]/g, '')
-  if (!number) return m.reply('❌ Numero non valido.')
-  let jid = number + '@s.whatsapp.net'
+  if (!text) return m.reply('❌ Inserisci un numero.\nEsempio:\n.fp +39 389 201 6995');
+  let number = text.replace(/[^0-9]/g, '');
+  if (!number) return m.reply('❌ Numero non valido.');
+  let jid = number + '@s.whatsapp.net';
   try {
-    let pp = await conn.profilePictureUrl(jid, 'image')
-      .catch(_ => 'https:                                             
-    await conn.sendFile(m.chat, pp, '//telegra.ph/file/24fa902ead26340f3df2c.png')
-    await conn.sendFile(m.chat, pp, 'fp.jpg', `📸 Foto profilo di:\n+${number}`)
+    let pp = await conn.profilePictureUrl(jid, 'image').catch(() => 'https:                                              
+    await conn.sendFile(m.chat, pp, '//telegra.ph/file/24fa902ead26340f3df2c.png');
+    await conn.sendFile(m.chat, pp, 'fp.jpg', `📸 Foto profilo di:\n+${number}`);
   } catch (e) {
-    m.reply('❌ Impossibile ottenere la foto profilo.')
+    m.reply('❌ Impossibile ottenere la foto profilo.');
   }
-}
-handler.help = ['fp <numero>']
-handler.tags = ['owner']
-handler.command = ['fp']
-handler.owner = true
-export default handler
+};
+handler.help = ['fp <numero>'];
+handler.tags = ['owner'];
+handler.command = ['fp'];
+handler.owner = true;
+export default handler;
